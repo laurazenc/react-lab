c01cfc5b0ca1935dc781f258c91acabcb5977e45
Laura <laurazenc@gmail.com>
2020-09-12 11:39:38 +0200

    Setup styled-components

--- github-jobs/pages/index.tsx
+++ github-jobs/pages/index.tsx
@@ -1,14 +1,9 @@
 import React from 'react'
-import Head from 'next/head'
 import Nav from '../components/nav'
 import Board from './board'
 
 const Home = () => (
   <div>
-    <Head>
-      <title>Home</title>
-      <link rel='icon' href='/favicon.ico' />
-    </Head>
 
     <Nav />
 
